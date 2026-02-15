b57cec34a7a2de4e460811d19043b168ea1833e9
Yarra Durga Phanindra <158799850+Phanindra2004@users.noreply.github.com>
2026-02-15 16:47:43 +0530

    Set up Express server with MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,34 @@
-const lib = require("./lib");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-module.exports = lib.default;
-Object.assign(module.exports, lib);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+// Schema
+const UserSchema = new mongoose.Schema({
+  name: String
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// API route
+app.get("/api/users", async (req,res)=>{
+  const users = await User.find();
+  res.json(users);
+});
+
+// Seed test data (run once)
+app.get("/seed", async(req,res)=>{
+  await User.create([{name:"chakri"}, {name:"phani"}]);
+  res.send("Seeded");
+});
+
+app.listen(5000, ()=> console.log("Server running on 5000"));
